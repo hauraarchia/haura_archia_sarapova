e04881ccaab5a559ba4e92250da31d21cb700ddc
Audric Andhika <audricc015@gmail.com>
2025-09-07 18:13:37 +0700

    Belajar Constanta dan Final

--- audric_andhika/Jobsheet_01/project1/bin/project1.dart
+++ audric_andhika/Jobsheet_01/project1/bin/project1.dart
@@ -1,5 +1,7 @@
 import 'package:project1/project1.dart' as project1;
 
+const String nama = "Audric";
+
 void main(List<String> arguments) {
   // print('Hello world: ${project1.calculate()}!');
   // satu line comment
@@ -7,4 +9,7 @@ void main(List<String> arguments) {
     multi line comment
     banyak line comment
   */
+  // constanta dan final
+  final String nama2 = "Andhika";
+  print("Nama saya $nama $nama2");
 }
