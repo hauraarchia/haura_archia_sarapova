4d119275fe75460f1628e5bea6c2acfa90d8a652
= <=>
2025-09-07 20:32:18 +0700

    const and final variable

--- husein_fadhlullah/jobsheet_1/project1/bin/project1.dart
+++ husein_fadhlullah/jobsheet_1/project1/bin/project1.dart
@@ -1,5 +1,6 @@
 import 'package:project1/project1.dart' as project1;
 
+const String nama = 'Husein';
 void main(List<String> arguments) {
   //print('Hello world: ${project1.calculate()}!');
   //satu line
@@ -7,6 +8,8 @@ void main(List<String> arguments) {
   multi line
   banyak line
   */
-
+  //constanta dan final variabel
+  final String nama2 = 'Affandi';
+  print("Nama saya $nama $nama2");
 
 }
