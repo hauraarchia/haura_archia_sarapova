377e92bc43affb82c1ef617d1deb4f1a54315cf9
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-09-07 23:41:12 +0700

    variabel

--- Lovelya_Sekarayu/jobsheet_1/project1/bin/project1.dart
+++ Lovelya_Sekarayu/jobsheet_1/project1/bin/project1.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:project1/project1.dart' as project1;
 
 const String nama = 'Lovelyta';
@@ -9,4 +11,21 @@ void main(List<String> arguments) {
   //constanta dan final
   final String nama2 = 'Sekarayu';
   print("Nama saya $nama $nama2");
+
+  //variabel
+  int umur = 45;
+  double tinggi = 165.5;
+  bool jenis_kelamin = false; //true = laki-laki, false = perempuan
+  String alamat = 'Jl. Merdeka No. 45';
+  stdout.writeln("Umur saya $umur tahun");
+  stdout.writeln("Tinggi saya $tinggi cm");
+  stdout.writeln(
+    "Jenis kelamin saya ${jenis_kelamin ? 'Laki-laki' : 'Perempuan'}"
+    );
+  // if (jenis_kelamin==true) {
+  //   stdout.writeln("Jenis kelamin saya Laki-laki");
+  // } else {
+  //   stdout.writeln("Jenis kelamin saya Perempuan");
+  // }
+  stdout.writeln("Alamat saya $alamat");
 }
