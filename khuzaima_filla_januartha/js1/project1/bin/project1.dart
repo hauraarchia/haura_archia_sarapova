7b2172849449e8aa9a6a9916ac38836cab673e59
khzmfl <khuzaimafilla@gmail.com>
2025-09-07 22:02:03 +0700

    CONSTANTA & FINAL

--- khuzaima_filla_januartha/js1/project1/bin/project1.dart
+++ khuzaima_filla_januartha/js1/project1/bin/project1.dart
@@ -1,5 +1,18 @@
 import 'package:project1/project1.dart' as project1;
 
+const String nama = "Filla";
+
 void main(List<String> arguments) {
-  print('Hello world: ${project1.calculate()}!');
+  //COMMENTS
+  // print('Hello world: ${project1.calculate()}!');
+  // satu line comment
+  /*
+    multi line comment
+    banyak line comment
+  */
+
+  //CONSTANTA & FINAL
+  final String nama2 = "Roblax";
+  print("Nama saya $nama $nama2");
+
 }
