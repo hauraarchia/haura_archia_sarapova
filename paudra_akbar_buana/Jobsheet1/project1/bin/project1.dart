5ddd753024a94faae7fa432f34a6eb8e438e0536
Pauaza <paudrabuana@gmail.com>
2025-09-07 19:29:07 +0700

    belajar variabel

--- paudra_akbar_buana/Jobsheet1/project1/bin/project1.dart
+++ paudra_akbar_buana/Jobsheet1/project1/bin/project1.dart
@@ -1,8 +1,10 @@
+import 'dart:io';
 import 'package:project1/project1.dart' as project1;
+
 const String nama = "Paudra";
 
 void main(List<String> arguments) {
-    // print('Hello world: ${project1.calculate()}!');
+  // print('Hello world: ${project1.calculate()}!');
   // satu line comment
   /*
     multi line comment
@@ -13,4 +15,29 @@ void main(List<String> arguments) {
   final String nama2 = "Akbar";
   print("Nama saya $nama $nama2");
 
+  // variabel
+  int umur = 21;
+  double tinggi = 176;
+  bool jenis_kelamin = true; // true = laki-laki, false = perempuan
+  String alamat = "Jl Tenaga Kepuharjo,Karangploso";
+  stdout.writeln("Umur saya $umur tahun");
+  stdout.writeln("Tinggi saya $tinggi cm");
+  stdout.writeln(
+    "Jenis kelamin saya ${jenis_kelamin ? "Laki-laki" : "Perempuan"}",
+  );
+  // if (jenis_kelamin == true) {
+  //   stdout.writeln("Jenis kelamin saya Laki-laki");
+  // } else {
+  //   stdout.writeln("Jenis kelamin saya Perempuan");
+  // }
+  stdout.writeln("Alamat saya $alamat");
+  num nilai = 90.5; // bisa menampung int dan double
+  stdout.writeln("Nilai saya $nilai");
+  dynamic bebas = "16";
+  stdout.writeln("Nilai dari dynamic adalah $bebas");
+  /*
+      String angka1 = "15";
+      int angka2 = int.parse(angka1);
+      int angka3 = bebas;
+    */
 }
