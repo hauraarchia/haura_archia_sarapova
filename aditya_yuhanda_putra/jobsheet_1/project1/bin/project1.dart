e9d9b81f3dd412d2e5956b5eb1e80dbebc365e6f
Yuanzth <adityayuhanda3001@gmail.com>
2025-09-07 15:00:37 +0700

    learn(dart): Constanta and Final

--- aditya_yuhanda_putra/jobsheet_1/project1/bin/project1.dart
+++ aditya_yuhanda_putra/jobsheet_1/project1/bin/project1.dart
@@ -1,5 +1,7 @@
 import 'package:project1/project1.dart' as project1;
 
+const String nama = 'Aditya';
+
 void main(List<String> arguments) {
   // print('Hello world: ${project1.calculate()}!');
   // satu line comment
@@ -7,4 +9,8 @@ void main(List<String> arguments) {
   multi line comment
   banyak line comment
   */
+
+  // Constanta dan Final
+  final String nama2 = 'Yuhanda';
+  print("Nama saya $nama $nama2");
 }
