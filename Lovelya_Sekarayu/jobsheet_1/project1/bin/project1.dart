f71372c445dd1d8a738a3a55c73c2bee26a0bf5b
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-09-07 23:30:16 +0700

    constanta dan final

--- Lovelya_Sekarayu/jobsheet_1/project1/bin/project1.dart
+++ Lovelya_Sekarayu/jobsheet_1/project1/bin/project1.dart
@@ -1,8 +1,12 @@
 import 'package:project1/project1.dart' as project1;
 
+const String nama = 'Lovelyta';
 void main(List<String> arguments) {
   //print('Hello world: ${project1.calculate()}!');
   //satu line comment
-  /* multi line comment * /
+  /* multi line comment */
   
+  //constanta dan final
+  final String nama2 = 'Sekarayu';
+  print("Nama saya $nama $nama2");
 }
