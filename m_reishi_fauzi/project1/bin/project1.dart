c4647d0bb886fff05821ca1050939f7d1b3f07ba
mrfauzii <reishifauzi24@gmail.com>
2025-09-07 13:42:15 +0700

    variabel

--- m_reishi_fauzi/project1/bin/project1.dart
+++ m_reishi_fauzi/project1/bin/project1.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:project1/project1.dart' as project1;
 const String nama = 'm reishi';
 
@@ -13,4 +15,23 @@ void main(List<String> arguments) {
   final String nama2 = 'fauzi';
 
   print("Nama saya $nama $nama2");
+
+  //VARIABEL
+  int umur = 100;
+  double tinggi = 172.5;
+  bool jenis_kelamin = false;
+  String alamat = "jln. DMC VII No.11 Bekasi";
+
+  stdout.writeln("umur saya $umur tahun");
+  stdout.writeln("tinggi saya $tinggi cm");
+  stdout.writeln("jenis kelamin saya ${jenis_kelamin ? 'laki-laki' : 'perempuan'}",
+    );
+  /*
+  if (jenis_kelamin==true) {
+    stdout.writeln('Jenis kelamin saya Laki-laki');
+  } else {
+    stdout.writeln('Jenis kelamin saya Perempuan');
+  }
+  */
+  stdout.writeln("alamat saya $alamat");
 }
