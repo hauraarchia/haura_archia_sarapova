64184b06d43fa976f4919cbec5762c6e46be5118
farelmaryamlailahajiri <farelhajiri295@gmail.com>
2025-09-07 21:07:48 +0700

    comments

--- farel_maryam_laila_hajiri/Jobsheet1/project1/bin/project1.dart
+++ farel_maryam_laila_hajiri/Jobsheet1/project1/bin/project1.dart
@@ -2,4 +2,11 @@ import 'package:project1/project1.dart' as project1;
 
 void main(List<String> arguments) {
   print('Hello world: ${project1.calculate()}!');
+
+  // 1. COMMENTS
+  // -----------------------
+  // Ini adalah single-line comment
+  /* Ini adalah 
+     multi-line comment */
+  /// Ini adalah documentation comment (biasanya untuk docs otomatis)
 }
