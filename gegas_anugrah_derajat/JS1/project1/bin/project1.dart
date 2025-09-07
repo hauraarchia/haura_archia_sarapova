ce774b27900a1b986469d66f9d3a41e217852703
Gegas96 <gegasanugrah@gmail.com>
2025-09-07 20:56:46 +0700

    belajar constanta dan final

--- gegas_anugrah_derajat/JS1/project1/bin/project1.dart
+++ gegas_anugrah_derajat/JS1/project1/bin/project1.dart
@@ -1,5 +1,5 @@
 import 'package:project1/project1.dart' as project1;
-
+const String nama = "Gegas";
 void main(List<String> arguments) {
   // print('Hello world: ${project1.calculate()}!');
   // satu line comment
@@ -7,4 +7,8 @@ void main(List<String> arguments) {
     multi line comment
     banyak line comment
   */
+
+  // constanta dan final
+  final String nama2 = "Anugrah";
+  print("Nama saya $nama $nama2");
 }
