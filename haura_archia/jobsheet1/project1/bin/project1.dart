692f7b4991e4cbfcc456b6f78317c9dfdc3f43da
haura archia sarapova putri kresna <hauraarchia@gmail.com>
2025-09-08 11:35:07 +0700

    variable

--- haura_archia/jobsheet1/project1/bin/project1.dart
+++ haura_archia/jobsheet1/project1/bin/project1.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:project1/project1.dart' as project1;
 
 const String nama = 'Haura';
@@ -10,8 +12,26 @@ void main(List<String> arguments) {
   multi line comment
   banyak komen line
   */
+
   // constanta dan final
   final String nama2 = 'Archia';
   // nama2 = "sara"; error, final tidak bisa diubah.
   print("Nama saya $nama $nama2");
+
+  //variable
+  int umur = 20;
+  double tinggi = 155.5;
+  bool jenis_kelamin = false; //true laki-laki, false perempuan
+  String alamat = 'Jl. Bunga andong timur No. 6a';
+  stdout.writeln("Umur saya $umur tahun");
+  stdout.writeln("Tinggi saya $tinggi cm");
+  stdout.writeln(
+    "Jenis kelamin saya ${jenis_kelamin ? 'Laki-laki' : 'Perempuan'}",
+  );
+  // if (jenis_kelamin == true) {
+  //   stdout.writeln("Jenis Kelamin saya Laki-laki");
+  // } else {
+  //   stdout.writeln("Jenis Kelamin saya Perempuan");
+  // }
+  stdout.writeln("Alamat saya $alamat");
 }
