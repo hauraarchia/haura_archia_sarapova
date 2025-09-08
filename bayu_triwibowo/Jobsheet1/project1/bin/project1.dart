0c4cf0c9436559d1ba690b87a97cd5c55cc3f460
BayuTriwibowo18 <wibowobayu600@gmail.com>
2025-09-08 11:07:48 +0700

    belajar koment

---
+++ bayu_triwibowo/Jobsheet1/project1/bin/project1.dart
@@ -0,0 +1,12 @@
+import 'package:project1/project1.dart' as project1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${project1.calculate()}!');
+ 
+
+  // untuk 1 line
+
+  /*untuk lebih dari 1 
+  line
+  */
+}
