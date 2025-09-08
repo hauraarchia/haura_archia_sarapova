2ef902a7ec14c6eb322e66d834b36602e8d947ac
Alvi Choirinnikmah <avialvi08@gmail.com>
2025-09-08 10:36:01 +0700

    Comments

--- alvi_choirinnikmah/jobsheet1/bin/jobsheet1.dart
+++ alvi_choirinnikmah/jobsheet1/bin/jobsheet1.dart
@@ -1,5 +1,11 @@
 import 'package:jobsheet1/jobsheet1.dart' as jobsheet1;
 
 void main(List<String> arguments) {
-  print('Hello world: ${jobsheet1.calculate()}!');
+  // print('Hello world: ${jobsheet1.calculate()}!');
+
+  // 1. Comments digunakan untuk memberikan penjelasan atau catatan kode 
+  // Line satu baris
+  /*
+    Multi line
+  */
 }
