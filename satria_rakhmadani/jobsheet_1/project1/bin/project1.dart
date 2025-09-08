c034d3918fd8cea30a6e91270ef153b9cfb31727
SatriaNadiRakhma <rakhmadanisatria@gmail.com>
2025-09-08 10:22:35 +0700

    variabel lanjut list dan map, serta exception

--- satria_rakhmadani/jobsheet_1/project1/bin/project1.dart
+++ satria_rakhmadani/jobsheet_1/project1/bin/project1.dart
@@ -29,4 +29,49 @@ void main(List<String> arguments) {
   //   stdout.writeln("Jenis kelamin saya Perempuan");
   // }
   stdout.writeln("Alamat saya $alamat");
+
+  //tipe data
+  num nilai = 100; //num bisa menampung int dan double
+  stdout.writeln("Nilai saya $nilai");
+  dynamic bebas = "16"; //dynamic bisa menampung semua tipe data
+  stdout.writeln("Nilai dari dynamic adalah $bebas");
+  // String angka1 = "15";
+  // int angka2 = int.parse(angka1);
+  // int angka3 = bebas;
+  // stdout.writeln("Hasil penjumlahan angka2 + angka3 = ${angka2 + angka3}");
+
+  //list
+  List<String> hobby = ["Mancing", "Tidur", "Ngoding", "Desain UI/UX", "Desain Grafis"];
+  hobby.add("Shopping");
+  stdout.writeln("Hobby saya adalah $hobby");
+
+  //map
+  Map<String, String> data = {
+    "nama": "Rakhmadani", 
+    "alamat": "Permata Tunggulwulung", 
+    "jenis_kelamin": "Laki-Laki", 
+  };
+  data["pekerjaan"] = "Mahasiswa";
+  data.addAll({"hobi": "Ngoding"});
+
+  stdout.writeln("Data: $data");
+  stdout.writeln("Nama yang ada di data ${data['nama']}");
+
+  void main() {
+  try {
+      // Code that might throw an exception
+      int result = 10 ~/ 0; // This will throw an IntegerDivisionByZeroException
+      print(result);
+    } on IntegerDivisionByZeroException {
+      // Handles the specific IntegerDivisionByZeroException
+      print("Cannot divide by zero!");
+    } catch (e) {
+      // Handles any other type of exception and provides the exception object
+      print("An unexpected error occurred: $e");
+    } finally {
+      // Code that always executes, regardless of whether an exception occurred
+      print("Execution complete.");
+    }
+  };
+
 }
