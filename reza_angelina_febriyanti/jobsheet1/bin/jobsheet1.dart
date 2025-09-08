44291034574396b06c843d9b3e7d54561ab57781
rzangef21 <2341760015@student.polinema.ac.id>
2025-09-08 12:30:19 +0700

    Belajar constanta & final

--- reza_angelina_febriyanti/jobsheet1/bin/jobsheet1.dart
+++ reza_angelina_febriyanti/jobsheet1/bin/jobsheet1.dart
@@ -1,6 +1,12 @@
 import 'package:jobsheet1/jobsheet1.dart' as jobsheet1;
 
+const String nama = 'Reza'; //Constanta
+
 void main(List<String> arguments) {
   //Komentar
   /*print('Hello world: ${jobsheet1.calculate()}!');*/
+
+  //Final
+  final String nama2 = 'Febriyanti';
+  print('Nama saya $nama $nama2');
 }
