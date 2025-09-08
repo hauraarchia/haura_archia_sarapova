b1952dfa40e1fd699872761590c6e5d4fedaf8bb
devitadwtar <devitadwilestari2@email.com>
2025-09-08 10:14:24 +0700

    Comments

--- DevitaDwiLestari/Jobsheet1/bin/jobsheet1.dart
+++ DevitaDwiLestari/Jobsheet1/bin/jobsheet1.dart
@@ -1,5 +1,15 @@
 import 'package:jobsheet1/jobsheet1.dart' as jobsheet1;
+import 'dart:io';
+
+// Constanta di luar main
+const String nama = 'Devita';
 
 void main(List<String> arguments) {
-  print('Hello world: ${jobsheet1.calculate()}!');
+  // Comments
+  // print('Tes program: ${jobsheet3.calculate()}');
+  // komentar satu baris
+  /*
+    contoh komentar panjang
+    lebih dari satu baris
+  */
 }
