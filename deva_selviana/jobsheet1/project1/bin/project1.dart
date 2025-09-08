dced55eb698d0294a012e5173490c7aded9d2ed6
devaselviana <selvianadeva8@gmail.com>
2025-09-08 09:16:57 +0700

    step variabel

--- deva_selviana/jobsheet1/project1/bin/project1.dart
+++ deva_selviana/jobsheet1/project1/bin/project1.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:project1/project1.dart' as project1;
 
 const String nama = 'Deva';
@@ -13,4 +15,26 @@ void main(List<String> arguments) {
   //constanta dan final
   final String nama2 = "Selviana";
   print("Nama saya $nama $nama2");
+  //variabel
+  int umur = 20;
+  double tinggi = 160;
+  bool jenis_kelamin = false; //true = laki-laki, false = perempuan
+  String alamat = "Jl. Semanggi barat no.15d";
+  stdout.writeln("Umur saya $umur tahun");
+  stdout.writeln("Tinggi saya $tinggi cm");
+  stdout.writeln("Jenis kelamin saya ${jenis_kelamin ? "Laki-laki" : "Perempuan"}");
+  stdout.writeln("Alamat saya $alamat");
+  // if (jenis_kelamin==true){
+  //   stdout.writeln ("Jenis kelamin saya Laki-laki");
+  // } else {
+  //   stdout.writeln ("Jenis kelamin saya Perempuan");
+  // }
+  stdout.writeln("Alamat saya $alamat");
+  num nilai = 90.5; //bisa diisi int atau double
+  stdout.writeln("Nilai saya $nilai");
+  dynamic bebas = "16";
+  stdout.writeln("Nilai dari dynamic adalah $bebas");
+  String angka1 = "15";
+  int angka2 = int.parse(angka1);
+  int angka3 = bebas;
 }
