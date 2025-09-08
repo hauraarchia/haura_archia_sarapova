9826c5be88331dd6858369936c229d1fe3d5463f
BayuTriwibowo18 <wibowobayu600@gmail.com>
2025-09-08 11:16:47 +0700

    belajar konstanta & final

--- bayu_triwibowo/Jobsheet1/project1/bin/project1.dart
+++ bayu_triwibowo/Jobsheet1/project1/bin/project1.dart
@@ -1,7 +1,7 @@
 import 'package:project1/project1.dart' as project1;
-
+const   String nama = "Bayu";
 void main(List<String> arguments) {
-  print('Hello world: ${project1.calculate()}!');
+  // print('Hello world: ${project1.calculate()}!');
  
 
   // untuk 1 line
@@ -9,4 +9,8 @@ void main(List<String> arguments) {
   /*untuk lebih dari 1 
   line
   */
+
+  // final dan constanta
+  final String nama2 = "Triwibowo";
+  print("Nama Saya $nama $nama2");
 }
