cb792bc0e34e4e37434f296885a1402a8c85d024
Izzatir Rofi'ah <zaizza294@gmail.com>
2025-09-08 09:55:30 +0700

    constanta dan final

--- IzzatirRofi'ah/project1/bin/project1.dart
+++ IzzatirRofi'ah/project1/bin/project1.dart
@@ -1,5 +1,6 @@
 import 'package:project1/project1.dart' as project1;
 
+const String name = 'Izzatir';
 void main(List<String> arguments) {
   // print('Hello world: ${project1.calculate()}!');
   // satu line comment
@@ -7,4 +8,7 @@ void main(List<String> arguments) {
   multi line comment
   banyak line comment
   */
+  // constanta dan final
+  final String name2 = 'Rofiah';
+  print("Nama Saya $name $name2");
 }
