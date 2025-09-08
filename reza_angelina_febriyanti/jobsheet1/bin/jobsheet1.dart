d82b522da36a062a05457fb8e75129f5d7a52c6a
rzangef21 <2341760015@student.polinema.ac.id>
2025-09-08 13:14:00 +0700

    Belajar tipe data

--- reza_angelina_febriyanti/jobsheet1/bin/jobsheet1.dart
+++ reza_angelina_febriyanti/jobsheet1/bin/jobsheet1.dart
@@ -19,6 +19,19 @@ void main(List<String> arguments) {
   print('Tinggi saya: $tinggi Meter');
 
   dynamic gaji = 150000000;
-  int gajiku = gaji;
+  num gajiku = gaji;
   stdout.writeln('Gaji saya $gajiku');
+  
+  String hobi = 'Traveling';
+  List<String> pekerjaan = ['Programmer', 'Konsultan'];
+  pekerjaan.add("Pengusaha");
+
+  stdout.writeln('Hobi saya $hobi');
+  stdout.writeln('Pekerjaan saya $pekerjaan');
+
+  Map<String, String> informasi = {
+    'email': 'reza.angelina@gmail.com',
+    'no_hp': '1234567890',
+  };
+  stdout.writeln('Informasi saya $informasi');
 }
