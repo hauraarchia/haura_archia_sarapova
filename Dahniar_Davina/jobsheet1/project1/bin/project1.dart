344114b02037c2be7d600194f91ad154262047bb
dahniardd <dahniardvn09@gmail.com>
2025-09-08 15:34:51 +0700

    try constanta & final

--- Dahniar_Davina/jobsheet1/project1/bin/project1.dart
+++ Dahniar_Davina/jobsheet1/project1/bin/project1.dart
@@ -1,9 +1,13 @@
 import 'package:project1/project1.dart' as project1;
 
+const String nama = "Dahniar";
 void main(List<String> arguments) {
   // print('Hello world: ${project1.calculate()}!');
   //satu line comment
   /* multi line comment
   banyak line comment */
 
+  //costanta dan final
+  final String nama2 = "Davina";
+  print("Nama Saya $nama $nama2");
 }
