38695bb9e97b30cce6d44297cf4e5127743afd5f
devaselviana <selvianadeva8@gmail.com>
2025-09-08 08:57:09 +0700

    step2 constanta dan final

--- deva_selviana/jobsheet1/project1/bin/project1.dart
+++ deva_selviana/jobsheet1/project1/bin/project1.dart
@@ -1,12 +1,16 @@
 import 'package:project1/project1.dart' as project1;
 
+const String nama = 'Deva';
 void main(List<String> arguments) {
   print('Hello world: ${project1.calculate()}!');
-  
+
 //print('Hello world: ${project1.calculate()}!');
     // satu line comment
   /*
     multi line comment
     banyak line comment
   */
+  //constanta dan final
+  final String nama2 = "Selviana";
+  print("Nama saya $nama $nama2");
 }
