dc2a211c592e5743e685786ce53e3a5156367a4c
haura archia sarapova putri kresna <hauraarchia@gmail.com>
2025-09-08 11:07:53 +0700

    comment

--- haura_archia/jobsheet1/project1/bin/project1.dart
+++ haura_archia/jobsheet1/project1/bin/project1.dart
@@ -1,5 +1,11 @@
 import 'package:project1/project1.dart' as project1;
 
 void main(List<String> arguments) {
-  print('Hello world: ${project1.calculate()}!');
+  //memberi komentar
+  // print('Hello world: ${project1.calculate()}!');
+  //satu line comment
+  /*
+  multi line comment
+  banyak komen line
+  */
 }
